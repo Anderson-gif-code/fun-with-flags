3b62832176c19b723e6a832c90a6ddd6c8c7e433
anderson.melo <awilliam.melo797@gmail.com>
2025-01-06 12:17:10 -0300

    Adding components and applyng tailwind in some parts

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="py-6 mb-8">
+            <p className="text-center">Made with S2 by Anderson Melo</p>
+        </footer>
+    );
+};
+
+export default Footer;
